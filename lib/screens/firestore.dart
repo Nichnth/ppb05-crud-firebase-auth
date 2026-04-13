17aa6682b373a8ead68972cf569c4bff3e6c39c8
Nicholas Abel <nicholasabel29@gmail.com>
2026-04-13 21:42:54 +0700

    add app

---
+++ lib/screens/firestore.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirestoreService{
+
+  final CollectionReference notes = FirebaseFirestore.instance.collection('notes');
+
+  //create new note
+  Future<void> addNote(String title, String content) {
+    return notes.add({
+      'title': title,
+      'content': content,
+      'createdAt': Timestamp.now(),
+    });
+  }
+
+  //fetch all notes
+  Stream<QuerySnapshot> getNotes() {
+    return notes.orderBy('createdAt', descending: true).snapshots();
+  }
+
+  //update notes
+  Future<void> updateNote(String id, String title, String content) {
+    return notes.doc(id).update({
+      'title': title,
+      'content': content,
+      'createdAt': Timestamp.now(),
+    });
+  }
+
+  //delete notes
+  Future<void> deleteNote(String id) {
+    return notes.doc(id).delete();
+  }
+
+}
